a59dab3e7b6151f691454e64c9beda6ef43b0975
Alex Ungurianu <iau2@st-andrews.ac.uk>
2016-08-23 13:39:29 +0100

    Postpone laziness for later, make it work first

--- infoarena/tasklist.py
+++ infoarena/tasklist.py
@@ -14,18 +14,15 @@ class TaskList(object):
         self.tasks = []
 
     def get_tasks(self,start,count):
-        if len(self.tasks) >= start+count and self.tasks[start] is not None and self.tasks[start+count-1] is not None:
-            return self.tasks[start:start+count]
-        else:
-            self._retrieve_task_segment(start,count)
-
-    def _retrieve_task_segment(self,start,count):
         slice_url = self.url.format(count=count,start=start)
         page_soup = get_soup_from_url(slice_url)
 
         task_table = page_soup.find(class_="tasks")
 
+        tasks = []
         for row in task_table.find("tbody").findAll("tr"):
             id = int(row.find(class_="number").get_text(),10)
             name = row.find(class_="task").find("a")["href"].split("/")[-1]
-            self.tasks[id] = Task(name)
+            tasks.append(Task(name))
+
+        return tasks
