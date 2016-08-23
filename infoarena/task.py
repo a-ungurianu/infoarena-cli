8b6b7897b217538f24d98a48630790d2e6b1a875
Alex Ungurianu <iau2@st-andrews.ac.uk>
2016-08-23 13:38:58 +0100

    Change task data from dict to object members

--- infoarena/task.py
+++ infoarena/task.py
@@ -6,35 +6,46 @@ PROBLEM_ROOT_URL = ROOT_URL + "/problema"
 class Task(object):
     def __init__(self,name):
         self.name = name
-        self.data = None
+        self.data_retrieved = False
 
     def get_description(self):
-        if self.data is None:
+        if not self.data_retrieved:
             self.retrieve_data()
-        return self.data["description"]
+        return self.description
+
+    def get_title(self):
+        if not self.data_retrieved:
+            self.retrieve_data()
+        return self.title
 
     def retrieve_data(self):
         url = PROBLEM_ROOT_URL + "/" + self.name
         page = get_soup_from_url(url)
-        self.data = {}
 
         problem_data_block = page.find(id="main").find(class_="wiki_text_block")
 
         problem_header = problem_data_block.h1
-        self.data["title"] = problem_header.get_text().strip()
+        self.title = problem_header.get_text().strip()
 
         problem_restriction_row = problem_data_block.table.findAll("tr")[2].findAll("td")
 
-        self.data["time"] = problem_restriction_row[1].get_text().strip()
-        self.data["memory"] = problem_restriction_row[3].get_text().strip()
+        self.time_limit = problem_restriction_row[1].get_text().strip()
+        self.memory_limit = problem_restriction_row[3].get_text().strip()
 
-        input_paragraph = problem_data_block.find("var",text=re.compile(self.name + ".in")).parent
-        self.data["input_description"] = input_paragraph.get_text().strip()
-        output_paragraph = problem_data_block.find("var",text=re.compile(self.name + ".out")).parent
-        self.data["output_description"] = output_paragraph.get_text().strip()
+        # Next sibling is called twice because the whitespace between them is considered as a tag
+        input_paragraph = problem_data_block.find("h2",text=re.compile("Date de in")).next_sibling.next_sibling
+        self.input_description = input_paragraph.get_text().strip()
+        output_paragraph = problem_data_block.find("h2",text=re.compile("Date de ie")).next_sibling.next_sibling
+        self.output_description = output_paragraph.get_text().strip()
 
-        self.data["description"] = ""
+        self.description = ""
         for paragraph in problem_header.find_next_siblings("p"):
             if paragraph == input_paragraph:
                 break
-            self.data["description"] += paragraph.get_text().strip() + "\n"
+
+            maybe_para_header = paragraph.previous_sibling.previous_sibling
+            if maybe_para_header.name == "h2":
+                self.description += "\n" + maybe_para_header.get_text() + "\n\n"
+            self.description += paragraph.get_text().strip() + "\n"
+
+        data_retrieved = True
